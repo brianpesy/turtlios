4b0013bdf7b8d7fa5323b27556b19b7bee466c49
Brian Sy <brianpesy@gmail.com>
2018-02-04 20:40:41 +0800

    Changed formatting for portrait mode

---
+++ Turtl/Turtl/BoardsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BoardsViewController.swift
+//  Turtl
+//
+//  Created by Nikki on 02/02/2018.
+//  Copyright © 2018 CS 192. All rights reserved.
+//
+
+import UIKit
+
+class BoardsViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var Boards: UIImageView!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var sharingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Turtl/Turtl/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Turtl
+//
+//  Created by Nikki on 01/02/2018.
+//  Copyright © 2018 CS 192. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var boardsButton: UIButton!
+    @IBOutlet weak var Main: UIImageView!
+    @IBOutlet weak var sharingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
